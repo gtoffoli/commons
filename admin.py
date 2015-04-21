8dac1972d43c67213bd640a6cc9e72709194e7da
Giovanni Toffoli <toffoli@linkroma.it>
2015-04-21 16:19:15 +0200

    Added models for OER (open educational resource). Fixed Repo.
    Added models OER, OerMetadata, OerTypeMetadataType and OerProxy.

--- admin.py
+++ admin.py
@@ -3,10 +3,28 @@ Created on 03/apr/2015
 @author: Giovanni Toffoli - LINK srl
 '''
 
+from django.db import models
+from django.forms import TextInput, Textarea
 from django.contrib import admin
+from tinymce.widgets import TinyMCE
 
-from .models import Subject, Language, RepoFeature, RepoType, Repo, ProjType, Project
+from .models import Subject, Language, ProjType, Project, RepoFeature, RepoType, Repo, OerMetadata, OER, OerTypeMetadataType, OerProxy
 from .forms import RepoForm, ProjectForm
+
+class ProjTypeAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['name', 'description',]}),
+    ]
+    list_display = ('name', 'description',)
+    search_fields = ['name',]
+
+class ProjAdmin(admin.ModelAdmin):
+    form = RepoForm
+    fieldsets = [
+        (None, {'fields': ['description', 'proj_type', 'info', 'eval',]}),
+    ]
+    list_display = ('description', 'proj_type', 'created', 'modified',)
+    search_fields = ['description', 'proj_type',]
 
 class LanguageAdmin(admin.ModelAdmin):
     fieldsets = [
@@ -24,9 +42,9 @@ class SubjectAdmin(admin.ModelAdmin):
 
 class RepoTypeAdmin(admin.ModelAdmin):
     fieldsets = [
-        (None, {'fields': ['name', 'description',]}),
+        (None, {'fields': ['name', 'description', 'order',]}),
     ]
-    list_display = ('name', 'description',)
+    list_display = ('name', 'description', 'order',)
     search_fields = ['name',]
 
 class RepoFeatureAdmin(admin.ModelAdmin):
@@ -38,33 +56,57 @@ class RepoFeatureAdmin(admin.ModelAdmin):
 class RepoAdmin(admin.ModelAdmin):
     form = RepoForm
     fieldsets = [
-        (None, {'fields': ['name', 'description', 'repo_type', 'url', 'features', 'subjects', 'languages', 'info', 'eval',]}),
+        (None, {'fields': ['name', 'description', 'url', 'repo_type', 'features', 'subjects', 'languages', 'info', 'eval',]}),
     ]
-    list_display = ('name', 'description', 'repo_type', 'url', 'created', 'modified',)
+    list_display = ('name', 'description', 'repo_type', 'url', 'user_fullname', 'created', 'modified',)
     search_fields = ['name', 'description', 'repo_type', 'features', 'subjects', 'languages',]
+    formfield_overrides = {
+       models.CharField: {'widget': TextInput(attrs={'class': 'span8'})},
+       models.TextField: {'widget': Textarea(attrs={'class': 'span8', 'rows': 2, 'cols': 80})},}
 
-class ProjTypeAdmin(admin.ModelAdmin):
-    fieldsets = [
-        (None, {'fields': ['name', 'description',]}),
-    ]
-    list_display = ('name', 'description',)
-    search_fields = ['name',]
+    def save_model(self, request, obj, form, change):
+        if not change and request.user:
+            obj.user = request.user
+        obj.save()
 
-class ProjAdmin(admin.ModelAdmin):
-    form = RepoForm
-    fieldsets = [
-        (None, {'fields': ['description', 'proj_type', 'info', 'eval',]}),
-    ]
-    list_display = ('description', 'proj_type', 'created', 'modified',)
-    search_fields = ['description', 'proj_type',]
+    def user_fullname(self, obj):
+        return obj.user.get_full_name()
 
-admin.site.register(Subject, SubjectAdmin)
+class OERAdmin(admin.ModelAdmin):
+    fieldsets = []
+    list_display = ('source', 'title', 'project', 'state', 'user_fullname', 'created',)
+    formfield_overrides = {
+       models.CharField: {'widget': TextInput(attrs={'class': 'span8'})},
+       models.TextField: {'widget': Textarea(attrs={'class': 'span8', 'rows': 2, 'cols': 80})},}
+
+    def save_model(self, request, obj, form, change):
+        if not change and request.user:
+            obj.user = request.user
+        obj.save()
+
+    def user_fullname(self, obj):
+        return obj.user.get_full_name()
+    
+class OerMetadataAdmin(admin.ModelAdmin):
+    fieldsets = []
+
+class OerTypeMetadataTypeAdmin(admin.ModelAdmin):
+    fieldsets = []
+
+class OerProxyAdmin(admin.ModelAdmin):
+    fieldsets = []
+
+admin.site.register(ProjType, ProjTypeAdmin)
+admin.site.register(Project, ProjAdmin)
 admin.site.register(Language, LanguageAdmin)
+admin.site.register(Subject, SubjectAdmin)
 admin.site.register(RepoFeature, RepoFeatureAdmin)
 admin.site.register(RepoType, RepoTypeAdmin)
 admin.site.register(Repo, RepoAdmin)
-admin.site.register(ProjType, ProjTypeAdmin)
-admin.site.register(Project, ProjAdmin)
+admin.site.register(OER, OERAdmin)
+admin.site.register(OerMetadata, OerMetadataAdmin)
+admin.site.register(OerTypeMetadataType, OerTypeMetadataTypeAdmin)
+admin.site.register(OerProxy, OerProxyAdmin)
 
 from django.contrib.flatpages.admin import FlatPageAdmin
 from django.contrib.flatpages.forms import FlatpageForm
--- models.py
+++ models.py
@@ -1,12 +1,12 @@
-# import pycountry
+from django.core.exceptions import ValidationError
+from django.core.validators import MinValueValidator
 from django.utils.translation import ugettext_lazy as _
 from django.db import models
 # from django import forms
-from django.contrib.flatpages.models import FlatPage
 from django.core.validators import URLValidator
-# from django.utils.text import slugify
 # from mayan import sources
-# from documents.settings import (LANGUAGE_CHOICES,)
+from metadata.models import MetadataType
+from documents.models import Document
 
 """ how to make the 'file' field optional in a DocumentVersion?
 import uuid
@@ -44,6 +44,7 @@ def create_favorites(sender, instance, created, **kwargs):
 """
 
 admin_user = User.objects.get(pk=1)
+admin_user_id = 1
 
 class Language(models.Model):
     """
@@ -74,94 +75,6 @@ class Subject(models.Model):
 
     def __unicode__(self):
         return self.name
-
-class RepoFeature(models.Model):
-    """
-    Define a repertoire of miscellaneous repository features
-    """
-    code = models.CharField(max_length=10, primary_key=True, verbose_name=_('code'))
-    name = models.CharField(max_length=100, verbose_name=_('name'))
-    order = models.PositiveIntegerField(default=0, verbose_name=_('sort order'))
-
-    class Meta:
-        verbose_name = _('repository feature')
-        verbose_name_plural = _('repository features')
-        ordering = ['order']
-
-    def __unicode__(self):
-        return self.name
-
-class RepoType(models.Model):
-    """
-    Define repository types
-    """
-    name = models.CharField(max_length=20, verbose_name=_('name'), unique=True)
-    # description = models.TextField(blank=True, null=True, verbose_name=_('description'))
-    description = models.CharField(max_length=100, verbose_name=_('description'))
-    order = models.PositiveIntegerField(default=0, verbose_name=_('sort order'))
-
-    class Meta:
-        verbose_name = _('repository type')
-        verbose_name_plural = _('repository types')
-        ordering = ['name']
-        ordering = ['order', 'name',]
-
-    def __unicode__(self):
-        return self.name
-
-    def natural_key(self):
-        return (self.name,)
-
-class Repo(models.Model):
-    repo_type = models.ForeignKey(RepoType, verbose_name=_('repository type'), related_name='repositories')
-    name = models.CharField(max_length=255, db_index=True, help_text=_('name used to identify the repository'), verbose_name=_('name'))
-    slug = AutoSlugField(unique=True, populate_from='name', editable=True)
-    url = models.CharField(max_length=64,  null=True, blank=True, verbose_name=_('URL of the repository site'), validators=[URLValidator()])
-    description = models.TextField(blank=True, null=True, verbose_name=_('short description'))
-    languages = models.ManyToManyField(Language, blank=True, verbose_name='languages of documents')
-    features = models.ManyToManyField(RepoFeature, blank=True, verbose_name='repository features')
-    subjects = models.ManyToManyField(Subject, blank=True, verbose_name='OER subjects')
-    # info_page = models.OneToOneField(FlatPage, null=True, blank=True, verbose_name=_('help page'), related_name='repository')
-    info = models.TextField(_('longer description'), blank=True, null=True)
-    eval = models.TextField(_('comments / evaluation'), blank=True, null=True)
-    created = CreationDateTimeField(_('created'))
-    modified = ModificationDateTimeField(_('modified'))
-    user = models.ForeignKey(User, verbose_name=_('last editor'))
-
-    class Meta:
-        verbose_name = _('repository')
-        verbose_name_plural = _('repositories')
-
-    def __unicode__(self):
-        return self.name
-
-    def type_description(self):
-        return self.repo_type.description
-
-    def save(self, *args, **kwargs):
-        if not self.user:
-            # user = request.user
-            user = admin_user
-            self.user = user
-        super(Repo, self).save(*args, **kwargs) # Call the "real" save() method.
-
-"""
-def repo_post_save(instance, created, raw, **kwargs):
-    ""
-    at creation, add info page (a flatpage)
-    ""
-    # Ignore fixtures and saves for existing repos.
-    if not created or raw:
-        return
-
-    if not instance.info_page_id:
-        info_page, _ = FlatPage.objects.get_or_create(url='/%s-info/' % instance.slug, title='Info on %s' % instance.name)
-        instance.info_page = info_page
-
-    instance.save()
-
-models.signals.post_save.connect(repo_post_save, sender=Repo, dispatch_uid='repo_post_save')
-"""
 
 class ProjType(models.Model):
     """
@@ -224,51 +137,225 @@ class Project(models.Model):
     def add_member(self, user):
         self.group.user_set.add(user)
 
-"""
-from sources.models import Source, InteractiveSource
-sources.literals.SOURCE_CHOICE_NULL_SOURCE = 'nullsource'
-from sources.literals import SOURCE_CHOICES, SOURCE_CHOICE_NULL_SOURCE
-SOURCE_CHOICES = list(SOURCE_CHOICES) + [(SOURCE_CHOICE_NULL_SOURCE, _('Null source')),]
-
-class NullSource(InteractiveSource):
-    is_interactive = True
-    source_type = SOURCE_CHOICE_NULL_SOURCE
-
-    def get_upload_file_object(self, form_data):
-        # return SourceUploadedFile(source=self, file=form_data['file'])
-        return None
+class RepoFeature(models.Model):
+    """
+    Define a repertoire of miscellaneous repository features
+    """
+    code = models.CharField(max_length=10, primary_key=True, verbose_name=_('code'))
+    name = models.CharField(max_length=100, verbose_name=_('name'))
+    order = models.PositiveIntegerField(default=0, verbose_name=_('sort order'))
 
     class Meta:
-        verbose_name = _('Null source')
-        verbose_name_plural = _('Null sources')
+        verbose_name = _('repository feature')
+        verbose_name_plural = _('repository features')
+        ordering = ['order']
 
-class NullSourceForm(forms.Form):
-    pass
+    def __unicode__(self):
+        return self.name
 
-import sources.utils
+class RepoType(models.Model):
+    """
+    Define repository types
+    """
+    name = models.CharField(max_length=20, verbose_name=_('name'), unique=True)
+    description = models.CharField(max_length=100, verbose_name=_('description'))
+    order = models.PositiveIntegerField(default=0, verbose_name=_('sort order'))
 
-old_get_class = sources.utils.get_class
-def get_class(source_type):
-    if source_type == SOURCE_CHOICE_NULL_SOURCE:
-        return NullSource
-    return old_get_class(source_type)
-sources.utils.old_get_class = old_get_class
-sources.utils.get_class = get_class
+    class Meta:
+        verbose_name = _('repository type')
+        verbose_name_plural = _('repository types')
+        ordering = ['name']
+        ordering = ['order', 'name',]
 
-old_get_form_class = sources.utils.get_form_class
-def get_form_class(source_type):
-    if source_type == SOURCE_CHOICE_NULL_SOURCE:
-        return NullSourceForm
-    return old_get_form_class(source_type)
-sources.utils.old_get_form_class = old_get_form_class
-sources.utils.get_form_class = get_form_class
+    def __unicode__(self):
+        return self.name
 
+    def natural_key(self):
+        return (self.name,)
 
-from navigation.api import register_links # , object_navigation
-from sources.permissions import PERMISSION_SOURCES_SETUP_CREATE
+    def option_label(self):
+        return '%s - %s' % (self.name, self.description)
 
-from sources.links import setup_sources, setup_source_create_webform, setup_source_create_staging_folder, setup_source_create_watch_folder, setup_source_create_pop3_email, setup_source_create_imap_email
-setup_source_create_nullsource = {'text': _('Add new null source'), 'view': 'sources:setup_source_create', 'args': '"%s"' % SOURCE_CHOICE_NULL_SOURCE, 'famfam': 'application_form_add', 'permissions': [PERMISSION_SOURCES_SETUP_CREATE], 'conditional_highlight': lambda context: context.get('source_type') == SOURCE_CHOICE_NULL_SOURCE and 'source' not in context}
-register_links([Source, 'sources:setup_source_list', 'sources:setup_source_create'], [setup_sources, setup_source_create_nullsource, setup_source_create_webform, setup_source_create_staging_folder, setup_source_create_pop3_email, setup_source_create_imap_email, setup_source_create_watch_folder], menu_name='secondary_menu')
-# register_links(Source, [setup_source_create_nullsource], menu_name='modified_menu')
+class Repo(models.Model):
+    repo_type = models.ForeignKey(RepoType, verbose_name=_('repository type'), related_name='repositories')
+    name = models.CharField(max_length=255, db_index=True, verbose_name=_('name'))
+    slug = AutoSlugField(unique=True, populate_from='name', editable=True)
+    url = models.CharField(max_length=64,  null=True, blank=True, verbose_name=_('URL of the repository site'), validators=[URLValidator()])
+    description = models.TextField(blank=True, null=True, verbose_name=_('short description'))
+    languages = models.ManyToManyField(Language, blank=True, verbose_name='languages of documents')
+    features = models.ManyToManyField(RepoFeature, blank=True, verbose_name='repository features')
+    subjects = models.ManyToManyField(Subject, blank=True, verbose_name='OER subjects')
+    # info_page = models.OneToOneField(FlatPage, null=True, blank=True, verbose_name=_('help page'), related_name='repository')
+    info = models.TextField(_('longer description'), blank=True, null=True)
+    eval = models.TextField(_('comments / evaluation'), blank=True, null=True)
+    created = CreationDateTimeField(_('created'))
+    modified = ModificationDateTimeField(_('modified'))
+    user = models.ForeignKey(User, verbose_name=_('last editor'))
+
+    class Meta:
+        verbose_name = _('repository')
+        verbose_name_plural = _('repositories')
+
+    def __unicode__(self):
+        return self.name
+
+    def type_description(self):
+        return self.repo_type.description
+
+    def save(self, *args, **kwargs):
+        if not self.user_id:
+            self.user_id = admin_user_id
+        super(Repo, self).save(*args, **kwargs) # Call the "real" save() method.
+
+# probably an OerType class is not necessary
+OER_TYPE_CHOICES = (
+    (0, '-'),
+    (1, 'metadata only'),
+    (2, 'metadata plus online reference'),
+    (3, 'document'),)
+
+OER_STATE_CHOICES = (
+    (0, 'new'),
+    (1, 'ok'),
+    (2, 'off'),)
+
+class OER(models.Model):
+    # oer_type = models.ForeignKey(OerType, verbose_name=_('OER type'), related_name='oers')
+    oer_type = models.IntegerField(choices=OER_TYPE_CHOICES,  validators=[MinValueValidator(1)], verbose_name='OER type')
+    source = models.ForeignKey(Repo, verbose_name=_('source repository'))
+    title = models.CharField(max_length=200, db_index=True, verbose_name=_('name'))
+    slug = AutoSlugField(unique=True, populate_from='title', editable=True)
+    url = models.CharField(max_length=64,  null=True, blank=True, help_text=_('URL to the OER in the source repository, if applicable'), validators=[URLValidator()])
+    reference = models.TextField(blank=True, null=True, verbose_name=_('reference'), help_text=_('other info used to identify and/or access the OER in the source repository'))
+    description = models.TextField(blank=True, null=True, verbose_name=_('short description'))
+    subjects = models.ManyToManyField(Subject, blank=True, verbose_name='OER subjects')
+    languages = models.ManyToManyField(Language, blank=True, verbose_name='languages of OER')
+    # derived_from = models.ManyToManyField('self', through='OerOer', symmetrical=False, verbose_name='derived from')
+    oers = models.ManyToManyField('self', symmetrical=False, blank=True, verbose_name='derived from')
+    project = models.ForeignKey(Project, help_text=_('where the OER has been cataloged or created'))
+    documents = models.ManyToManyField(Document, blank=True, verbose_name='attached documents')
+    # state = models.ForeignKey(OerState, verbose_name=_('OER state'))
+    state = models.IntegerField(choices=OER_STATE_CHOICES, default=0, null=True, verbose_name='OER state')
+    created = CreationDateTimeField(_('created'))
+    modified = ModificationDateTimeField(_('modified'))
+    user = models.ForeignKey(User, default=1, verbose_name=_('last editor'))
+
+    def __unicode__(self):
+        return self.title
+
+    def save(self, *args, **kwargs):
+        if not self.user_id:
+            self.user_id = admin_user_id
+        super(Repo, self).save(*args, **kwargs) # Call the "real" save() method.
+
+class OerMetadata(models.Model):
+    """
+    Link an OER to a specific instance of a metadata type with it's current value
+    """
+    oer = models.ForeignKey(OER, related_name='metadata', verbose_name=_('OER'))
+    metadata_type = models.ForeignKey(MetadataType, verbose_name=_('Type'))
+    value = models.CharField(max_length=255, blank=True, null=True, verbose_name=_('Value'), db_index=True)
+    modified = ModificationDateTimeField(_('modified'))
+    user = models.ForeignKey(User, verbose_name=_('last editor'))
+
+    def __unicode__(self):
+        return unicode(self.metadata_type)
+
+    def save(self, *args, **kwargs):
+        if self.metadata_type.pk not in self.oer.oer_type.metadata.values_list('metadata_type', flat=True):
+            raise ValidationError(_('Metadata type is not valid for this OER type.'))
+
+        return super(OerMetadata, self).save(*args, **kwargs)
+
+    def delete(self, enforce_required=True, *args, **kwargs):
+        if enforce_required and self.metadata_type.pk in self.oer.oer_type.metadata.filter(required=True).values_list('metadata_type', flat=True):
+            raise ValidationError(_('Metadata type is required for this oer type.'))
+
+        return super(OerMetadata, self).delete(*args, **kwargs)
+
+    class Meta:
+        unique_together = ('oer', 'metadata_type')
+        verbose_name = _('OER metadata')
+        verbose_name_plural = _('OER metadata')
+
+class OerTypeMetadataType(models.Model):
+    # oer_type = models.ForeignKey(OerType, related_name='metadata', verbose_name=_('OER type'))
+    oer_type = models.IntegerField(choices=OER_TYPE_CHOICES, default=0, null=True, verbose_name='OER type')
+    metadata_type = models.ForeignKey(MetadataType, verbose_name=_('Metadata type'))
+    required = models.BooleanField(default=False, verbose_name=_('Required'))
+
+    def __unicode__(self):
+        return unicode(self.metadata_type)
+
+    class Meta:
+        unique_together = ('oer_type', 'metadata_type')
+        verbose_name = _('metadata type option for OER type')
+        verbose_name_plural = _('metadata type options for OER type')
+
+""" OER Evaluations will be user volunteered paradata
+from metadata.settings import AVAILABLE_VALIDATORS # ignore parse time error
+
+class EvaluationTypeManager(models.Manager):
+    def get_by_natural_key(self, name):
+        return self.get(name=name)
+
+# Similar to metadata type (see mayan.metadata)
+class EvaluationType(models.Model):
+    # Define a type of evaluation (see MetadataType in metadata.models
+    name = models.CharField(unique=True, max_length=48, verbose_name=_('Name'), help_text=_('Do not use python reserved words, or spaces.'))
+    # TODO: normalize 'title' to 'label'
+    title = models.CharField(max_length=48, verbose_name=_('Title'))
+    default = models.CharField(max_length=128, blank=True, null=True,
+                               verbose_name=_('Default'),
+                               help_text=_('Enter a string to be evaluated.'))
+    # TODO: Add enable_lookup boolean to allow users to switch the lookup on and
+    # off without losing the lookup expression
+    lookup = models.TextField(blank=True, null=True,
+                              verbose_name=_('Lookup'),
+                              help_text=_('Enter a string to be evaluated that returns an iterable.'))
+    validation = models.CharField(blank=True, choices=zip(AVAILABLE_VALIDATORS, AVAILABLE_VALIDATORS), max_length=64, verbose_name=_('Validation function name'))
+    # TODO: Find a different way to let users know what models and functions are
+    # available now that we removed these from the help_text
+    objects = EvaluationTypeManager()
+
+    def __unicode__(self):
+        return self.title
+
+    def natural_key(self):
+        return (self.name,)
+
+    class Meta:
+        ordering = ('title',)
+        verbose_name = _('Evaluation type')
+        verbose_name_plural = _('Evaluation types')
+
+class OerEvaluation(models.Model):
+    # Link an OER to a specific instance of an evaluation type with it's current value
+    oer = models.ForeignKey(OER, related_name='evaluation', verbose_name=_('OER'))
+    evaluation_type = models.ForeignKey(EvaluationType, verbose_name=_('Type'))
+    value = models.CharField(max_length=255, blank=True, null=True, verbose_name=_('Value'), db_index=True)
+    modified = ModificationDateTimeField(_('modified'))
+    user = models.ForeignKey(User, verbose_name=_('last editor'))
+
+    def __unicode__(self):
+        return unicode(self.evaluation_type)
+
+    class Meta:
+        unique_together = ('oer', 'evaluation_type', 'user')
+        verbose_name = _('OER evaluation')
+        verbose_name_plural = _('OER evaluations')
 """
+""" not necessary ? use the Project class, instead
+class OerFolder(models.Model):
+    name = models.CharField(max_length=255, db_index=True, help_text=_('name used to identify the OER folder'), verbose_name=_('name'))
+    slug = AutoSlugField(unique=True, populate_from='name', editable=True)
+    created = CreationDateTimeField(_('created'))
+    user = models.ForeignKey(User, verbose_name=_('last editor'))
+"""
+
+class OerProxy(models.Model):
+    oer = models.ForeignKey(OER, verbose_name=_('stands for'))
+    # folder = models.ForeignKey(OerFolder, verbose_name=_('OER folder'))
+    project = models.ForeignKey(Project, verbose_name=_('project'))
+    created = CreationDateTimeField(_('created'))
+    user = models.ForeignKey(User, verbose_name=_('last editor'))
