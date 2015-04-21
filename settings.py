7a1ae59e9e79b5f8294ee124fe6c0058b438265b
Giovanni Toffoli <toffoli@linkroma.it>
2015-04-21 18:18:16 +0200

    Restructured the settings module

--- settings.py
+++ settings.py
@@ -3,6 +3,12 @@ Django settings for commons project.
 """
 
 from mayan.settings.base import *
+from private import *
+
+if PRODUCTION:
+    DEBUG = False
+else:
+    DEBUG = True
 
 # ========= EXTENSIONS BY COMMONS
 
@@ -33,7 +39,8 @@ TEMPLATE_CONTEXT_PROCESSORS = list(TEMPLATE_CONTEXT_PROCESSORS) + [
 ]
 
 # in development, disable template caching
-if os.name == 'nt':
+# if os.name == 'nt':
+if not PRODUCTION:
     TEMPLATE_LOADERS = (
         'django.template.loaders.filesystem.Loader',
         'django.template.loaders.app_directories.Loader',
@@ -112,7 +119,35 @@ if os.name == 'nt':
 else:
     COMMON_TEMPORARY_DIRECTORY = '/tmp'
 
-from private import *
+# A sample logging configuration. The only tangible logging
+# performed by this configuration is to send an email to
+# the site admins on every HTTP 500 error when DEBUG=False.
+# See http://docs.djangoproject.com/en/dev/topics/logging for
+# more details on how to customize your logging configuration.
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse'
+        }
+    },
+    'handlers': {
+        'mail_admins': {
+            'level': 'ERROR',
+            'filters': ['require_debug_false'],
+            'class': 'django.utils.log.AdminEmailHandler'
+        }
+    },
+    'loggers': {
+        'django.request': {
+            'handlers': ['mail_admins'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+    }
+}
+
 
 # --------- HIERARCHICAL GROUPS ----------------
 AUTHENTICATION_BACKENDS = (
