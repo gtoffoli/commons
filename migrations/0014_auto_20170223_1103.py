9a2d968b7ce39ad051066930ee462922b9c3b913
Giovanni Toffoli <toffoli@linkroma.it>
2018-11-29 14:13:15 +0100

    In 3 migration files fixed AutoSlugField populate_from

--- migrations/0014_auto_20170223_1103.py
+++ migrations/0014_auto_20170223_1103.py
@@ -30,7 +30,7 @@ class Migration(migrations.Migration):
         migrations.AlterField(
             model_name='project',
             name='slug',
-            field=django_extensions.db.fields.AutoSlugField(editable=False, populate_from=b'name', blank=True, unique=True, overwrite=True),
+            field=django_extensions.db.fields.AutoSlugField(editable=False, populate_from='name', blank=True, unique=True, overwrite=True),
         ),
         migrations.AlterField(
             model_name='userprofile',
--- migrations/0016_auto_20170223_1149.py
+++ migrations/0016_auto_20170223_1149.py
@@ -15,21 +15,21 @@ class Migration(migrations.Migration):
         migrations.AlterField(
             model_name='learningpath',
             name='slug',
-            field=django_extensions.db.fields.AutoSlugField(editable=False, populate_from=b'title', max_length=80, blank=True, unique=True, overwrite=True),
+            field=django_extensions.db.fields.AutoSlugField(editable=False, populate_from='title', max_length=80, blank=True, unique=True, overwrite=True),
         ),
         migrations.AlterField(
             model_name='oer',
             name='slug',
-            field=django_extensions.db.fields.AutoSlugField(editable=False, populate_from=b'title', max_length=80, blank=True, unique=True, overwrite=True),
+            field=django_extensions.db.fields.AutoSlugField(editable=False, populate_from='title', max_length=80, blank=True, unique=True, overwrite=True),
         ),
         migrations.AlterField(
             model_name='project',
             name='slug',
-            field=django_extensions.db.fields.AutoSlugField(editable=False, populate_from=b'name', max_length=80, blank=True, unique=True, overwrite=True),
+            field=django_extensions.db.fields.AutoSlugField(editable=False, populate_from='name', max_length=80, blank=True, unique=True, overwrite=True),
         ),
         migrations.AlterField(
             model_name='repo',
             name='slug',
-            field=django_extensions.db.fields.AutoSlugField(editable=False, populate_from=b'name', max_length=80, blank=True, unique=True, overwrite=True),
+            field=django_extensions.db.fields.AutoSlugField(editable=False, populate_from='name', max_length=80, blank=True, unique=True, overwrite=True),
         ),
     ]
