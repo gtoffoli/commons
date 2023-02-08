f405e6ce720684f82f808de204b928b63270c32a
Giovanni Toffoli <toffoli@linkroma.it>
2023-02-08 18:31:13 +0100

    Extended set of file extensions for attachments.

--- commons/settings.py
+++ commons/settings.py
@@ -602,7 +602,7 @@ HOMEPAGE_TIMEOUT = 60 * 60 * 24 # seconds in 1 day
 RECENT_HOURS = 24 # after this time posts and messages are no more new
 
 #file attachment
-EXTS_FILE_ATTACHMENT = 'txt|doc|docx|ppt|pptx|pdf|xls|xlsx|odt|odp|ods|jpg|png|mp3|mp4|rtf|ipynb|zip'
+EXTS_FILE_ATTACHMENT = 'txt|doc|docx|ppt|pptx|pdf|xls|xlsx|odt|odp|ods|jpg|png|mp3|mp4|rtf|ipynb|zip|apk'
 SIZE_FILE_ATTACHMENT = 10
 EXTS_FILE_USER_PROFILE = 'pdf'
 SIZE_FILE_USER_PROFILE = 2
