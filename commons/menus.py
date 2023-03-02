600a13a0cbfb931031964ec2c80d21a38e685c18
Giovanni Toffoli <toffoli@linkroma.it>
2023-03-02 14:28:07 +0100

    Added Student Feedback App in menu.

--- commons/menus.py
+++ commons/menus.py
@@ -137,6 +137,11 @@ def my_children(request):
          capfirst(_("text analysis")),
          url='/textanalysis/ta_input/',
         ))
+    if settings.SITE_ID in [1, 5]:
+        children.append (MenuItem(
+             capfirst(_("student feedback")),
+             url='/feedback/attendee/',
+            ))
     return children
 
 def help_children(request):
