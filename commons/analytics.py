579ad81d0cc57bdea2708110b8bde7bd45fdd7c8
Giovanni Toffoli <toffoli@linkroma.it>
2023-09-08 13:00:03 +0200

    Fixed role check in view/search of activity stream.

--- commons/analytics.py
+++ commons/analytics.py
@@ -295,7 +295,8 @@ def filter_actions(user=None, verbs=[], object_content_type=None, project=None, 
     return actions
 
 def activity_stream(request, user=None, max_actions=100, max_days=30):
-    if not request.user.is_authenticated or not request.user.is_manager():
+    # if not request.user.is_authenticated or not request.user.is_manager():
+    if not request.user.is_authenticated or (user!=request.user and not request.user.is_manager()):
         return HttpResponseForbidden()
     actions = []
     if user==request.user or request.user.is_superuser or (request.user.is_authenticated and request.user.is_manager(1)):
